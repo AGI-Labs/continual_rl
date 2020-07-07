ec3da5d1fd6bc17c5e7c87db28378cb34a7a9d4d
Sam Powers <Sam.N.Powers@gmail.com>
2020-07-07 15:48:34 -0400

    Remove ImageTask and PPO, these will get added in later.

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -1,16 +1,9 @@
 from continual_rl.experiments.experiment import Experiment
-from continual_rl.experiments.tasks.image_task import ImageTask
 from continual_rl.experiments.tasks.minigrid_task import MiniGridTask
 
 
 def get_available_experiments(output_dir):
     experiments = {
-        "breakout":
-            Experiment(tasks=[
-                ImageTask(env_spec='BreakoutDeterministic-v4', num_timesteps=10000000, time_batch_size=4,
-                          eval_mode=False, output_dir=output_dir, image_size=[84, 84], grayscale=True)
-            ], output_dir=output_dir),
-
         "recall_minigrid_empty8x8_unlock_empty8x8":
             Experiment(tasks=[MiniGridTask(env_spec='MiniGrid-Empty-8x8-v0', num_timesteps=150000, time_batch_size=1,
                                            eval_mode=False, output_dir=output_dir),
--- continual_rl/policies/random_policy/random_policy.py
+++ continual_rl/policies/random_policy/random_policy.py
@@ -3,7 +3,6 @@ from continual_rl.policies.policy_base import PolicyBase
 from continual_rl.policies.random_policy.random_policy_config import RandomPolicyConfig
 from continual_rl.policies.random_policy.random_info_to_store import RandomInfoToStore
 from continual_rl.experiments.environment_runners.environment_runner_sync import EnvironmentRunnerSync
-from continual_rl.experiments.environment_runners.environment_runner_batch import EnvironmentRunnerBatch
 
 
 class RandomPolicy(PolicyBase):
@@ -16,18 +15,11 @@ class RandomPolicy(PolicyBase):
         self._config = config
 
     def get_environment_runner(self):
-        if self._config.num_parallel_envs is None:
-            runner = EnvironmentRunnerSync(policy=self, timesteps_per_collection=self._config.timesteps_per_collection)
-        else:
-            runner = EnvironmentRunnerBatch(policy=self, num_parallel_envs=self._config.num_parallel_envs,
-                                            timesteps_per_collection=self._config.timesteps_per_collection)
+        runner = EnvironmentRunnerSync(policy=self, timesteps_per_collection=self._config.timesteps_per_collection)
         return runner
 
     def compute_action(self, observation, task_action_count):
-        if self._config.num_parallel_envs is None:
-            action = random.choice(range(task_action_count))
-        else:
-            action = random.choice(range(task_action_count), self._config.num_parallel_envs)
+        action = random.choice(range(task_action_count))
 
         return action, RandomInfoToStore()
 
