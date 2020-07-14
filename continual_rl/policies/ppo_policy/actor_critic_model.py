Fixed advantage calculation - wasn't actually updating next_advantage and next_value. Coming soon (hopefully) unit tests. Also some windows-specific stuff, and making sure cuda()-ness is propagated correctly.

continual_rl/policies/ppo_policy/actor_critic_model.py
@@ -1,13 +1,14 @@
 import torch.nn as nn
 from torch_ac.model import ACModel
 from torch.distributions.categorical import Categorical
+import torch.nn.functional as F
 from continual_rl.utils.common_exceptions import ObservationShapeNotRecognized
 
 
 class ConvNet7x7(nn.Module):  # TODO: lazily using the existing one as a base, extract out
     def __init__(self):
         super().__init__()
-        self.output_size = 32  # Consistent, I think, with the minigrid rl benchmark
+        self.output_size = 64  # Consistent, I think, with the minigrid rl benchmark
 
         # From: https://github.com/lcswillems/rl-starter-files/blob/master/model.py
         # TODO: temp, or include license?
@@ -17,7 +18,7 @@ class ConvNet7x7(nn.Module):  # TODO: lazily using the existing one as a base, e
             nn.MaxPool2d((2, 2)),
             nn.Conv2d(16, 32, (2, 2)),  # 32
             nn.ReLU(),
-            nn.Conv2d(32, 32, (2, 2)),  # 32, 64
+            nn.Conv2d(32, 64, (2, 2)),  # 32, 64
             nn.ReLU(),
             nn.Flatten()
         )
@@ -64,8 +65,16 @@ class ActorCritic(nn.Module, ACModel):
         else:
             raise ObservationShapeNotRecognized(f"Observation shape {observation_space[1:]} not found")
 
-        self._actor = nn.Linear(self._embedding.output_size, action_space)
-        self._critic = nn.Linear(self._embedding.output_size, 1)
+        self._actor = nn.Sequential(
+            nn.Linear(self._embedding.output_size, 64),
+            nn.Tanh(),
+            nn.Linear(64, action_space)
+        )
+        self._critic = nn.Sequential(
+            nn.Linear(self._embedding.output_size, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1)
+        )
 
         self._task_action_count = None
 
@@ -82,6 +91,6 @@ class ActorCritic(nn.Module, ACModel):
         actor_result = self._actor(embedding)
         critic_result = self._critic(embedding)
 
-        distribution = Categorical(logits=actor_result[:, :task_action_count])  # TODO: F.log_softmax()?
+        distribution = Categorical(logits=F.log_softmax(actor_result[:, :task_action_count], dim=1))
 
         return distribution, critic_result

continual_rl/policies/ppo_policy/ppo_policy.py
@@ -128,6 +128,8 @@ class PPOPolicy(PolicyBase):
 
             delta = info_entry.reward + self._config.discount * next_value - info_entry.value
             advantages[entry_id] = delta + self._config.discount * self._config.gae_lambda * next_advantage
+            next_value = info_entry.value
+            next_advantage = advantages[entry_id]
 
         return advantages
 
@@ -148,27 +150,26 @@ class PPOPolicy(PolicyBase):
         all_log_probs = []
 
         for env_data in condensed_env_sorted:
-            if self._config.use_cuda:
-                env_data = [data.cuda() for data in env_data]
+            env_data = [data.to_tensors(self._config.use_cuda) for data in env_data]
 
             all_observations.append([entry.observation for entry in env_data])
-            all_actions.append([entry.action for entry in env_data])
-            all_values.append([entry.value for entry in env_data])
-            all_rewards.append([entry.reward for entry in env_data])
-            all_advantages.append(self._compute_advantages(env_data))
-            all_log_probs.append([entry.log_prob for entry in env_data])
+            all_actions.append(torch.stack([entry.action for entry in env_data]))
+            all_values.append(torch.stack([entry.value for entry in env_data]))
+            all_rewards.append(torch.stack([entry.reward for entry in env_data]))
+            all_advantages.append(torch.stack(self._compute_advantages(env_data)))
+            all_log_probs.append(torch.stack([entry.log_prob for entry in env_data]))
 
         # torch_ac's experiences expect [num_envs, timesteps_per_collection] -> [num_envs * timesteps_per_collection]
         # Thanks to torch_ac for this PPO implementation - LICENSE available as a sibling to this file
         experiences = DictList()
         experiences.obs = torch.stack([all_observations[j][i]
                                        for j in range(self._config.num_parallel_envs)
-                                       for i in range(self._config.timesteps_per_collection)])
-        experiences.action = torch.Tensor(all_actions).reshape(-1)
-        experiences.value = torch.Tensor(all_values).reshape(-1)
-        experiences.reward = torch.Tensor(all_rewards).reshape(-1)
-        experiences.advantage = torch.Tensor(all_advantages).reshape(-1)
-        experiences.log_prob = torch.Tensor(all_log_probs).reshape(-1)
+                                       for i in range(self._config.timesteps_per_collection)]).detach()
+        experiences.action = torch.stack(all_actions).reshape(-1).detach()
+        experiences.value = torch.stack(all_values).reshape(-1).detach()
+        experiences.reward = torch.stack(all_rewards).reshape(-1).detach()
+        experiences.advantage = torch.stack(all_advantages).reshape(-1).detach()
+        experiences.log_prob = torch.stack(all_log_probs).reshape(-1).detach()
 
         experiences.returnn = experiences.value + experiences.advantage
 