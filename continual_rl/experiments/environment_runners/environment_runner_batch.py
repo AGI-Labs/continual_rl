b075a7f79741370563fad7791ca9bdebae1c5495
Sam Powers <Sam.N.Powers@gmail.com>
2020-07-19 01:47:58 -0400

    Not all observations were getting reset before (TODO: unit test), and also early stopping wasn't triggering a reset because that was happening internal to ParallelEnv. I don't love that I just implemented some essentially ParallelEnv internals in this function... Might write my own in the near future.

--- continual_rl/experiments/environment_runners/environment_runner_batch.py
+++ continual_rl/experiments/environment_runners/environment_runner_batch.py
@@ -33,16 +33,40 @@ class EnvironmentRunnerBatch(EnvironmentRunnerBase):
     def _preprocess_raw_observations(self, preprocessor, raw_observations):
         return torch.stack([preprocessor(raw_observation) for raw_observation in raw_observations])
 
-    def _reset_env(self, time_batch_size, preprocessor):
+    def _initialize_envs(self, env_spec, time_batch_size, preprocessor):
+        envs = [Utils.make_env(env_spec) for _ in range(self._num_parallel_envs)]
+        self._parallel_env = ParallelEnv(envs)
+
         # Initialize the observation time-batch with n of the first observation.
         raw_observations = self._parallel_env.reset()
         processed_observations = self._preprocess_raw_observations(preprocessor, raw_observations)
 
+        self._observations = self._initialize_observations(processed_observations, time_batch_size)
+
+    def _reset_env(self, env_id):
+        """
+        ParallelEnv doesn't readily expose manually resetting an environment, so doing that here.
+        """
+        if env_id == 0:
+            observation = self._parallel_env.envs[0].reset()
+        else:
+            local = self._parallel_env.locals[env_id-1]
+            local.send(("reset", None))
+            observation = local.recv()
+
+        return observation
+
+    def _initialize_observations(self, processed_observations, time_batch_size):
         observations = deque(maxlen=time_batch_size)
+
         for _ in range(time_batch_size):
             observations.append(processed_observations)
 
         return observations
+
+    def _reset_observations_for_env(self, reset_observation, time_batch_size, env_id):
+        for time_id in range(time_batch_size):
+            self._observations[time_id][env_id] = reset_observation
 
     def collect_data(self, time_batch_size, env_spec, preprocessor, task_id, episode_renderer=None,
                      early_stopping_condition=None):
@@ -55,13 +79,9 @@ class EnvironmentRunnerBatch(EnvironmentRunnerBase):
         logs_to_report = []  # {tag, type ("video", "scalar"), value, timestep}
 
         if self._parallel_env is None:
-            envs = [Utils.make_env(env_spec) for _ in range(self._num_parallel_envs)]
-            self._parallel_env = ParallelEnv(envs)
+            self._initialize_envs(env_spec, time_batch_size, preprocessor)
 
         for timestep_id in range(self._timesteps_per_collection):
-            if self._observations is None:
-                self._observations = self._reset_env(time_batch_size, preprocessor)
-
             stacked_observations = torch.stack(list(self._observations), dim=1)
             actions, info_to_store = self._policy.compute_action(stacked_observations,
                                                                  task_id,
@@ -70,20 +90,34 @@ class EnvironmentRunnerBatch(EnvironmentRunnerBase):
             # ParallelEnv automatically resets the env and returns the new observation when a "done" occurs
             result = self._parallel_env.step(actions)
             raw_observations, rewards, dones, infos = list(result)
+            dones = list(dones)  # To enable item assignment in early-stopping
 
             self._total_timesteps += self._num_parallel_envs
-
-            if early_stopping_condition is not None:
-                dones |= np.array([early_stopping_condition(self._total_timesteps, infos[env_id])
-                                   for env_id in range(self._num_parallel_envs)])
-
             self._observations.append(self._preprocess_raw_observations(preprocessor, raw_observations))
             self._last_info_to_store = info_to_store
             self._cumulative_rewards += np.array(rewards)
             self._observations_to_render.append(self._observations[-1][0])  # Take the most recent first env's observation
 
+            # Update our dones according to the early_stopping_condition, if applicable
+            # "Normal" done resets happen in the ParallelEnv. Here we're forcing one, so replicate the behavior:
+            # Send a reset message to the process, get the new observation.
+            # Replace the last observation in self._observations with this new observation.
+            # Then we'll trigger a full observation-reset in the next loop
+            for env_id, done in enumerate(dones):
+                if early_stopping_condition is not None:
+                    stop_early = early_stopping_condition(self._total_timesteps, infos[env_id])
+                    dones[env_id] |= stop_early
+
+                    if stop_early:
+                        new_observation = preprocessor(self._reset_env(env_id))
+                        self._observations[-1][env_id] = new_observation
+
             for env_id, done in enumerate(dones):
                 if done:
+                    # The last observation was populated from the new environment. Grab it and reset the rest from it.
+                    new_observation = self._observations[-1][env_id]
+                    self._reset_observations_for_env(new_observation, time_batch_size, env_id)
+
                     rewards_to_report.append(self._cumulative_rewards[env_id])
                     self._cumulative_rewards[env_id] *= 0  # TODO: lazy...
 
