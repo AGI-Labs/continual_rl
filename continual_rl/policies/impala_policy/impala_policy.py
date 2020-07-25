31b352a61b35d4b7567839d2ebc71e73064116c5
Sam Powers <Sam.N.Powers@gmail.com>
2020-07-24 21:21:52 -0400

    A couple simple hooks.

--- continual_rl/policies/impala_policy/impala_policy.py
+++ continual_rl/policies/impala_policy/impala_policy.py
@@ -1,5 +1,6 @@
 from continual_rl.policies.policy_base import PolicyBase
-from continual_rl.policies.impala_policy import ImpalaPolicyConfig
+from continual_rl.policies.impala_policy.impala_policy_config import ImpalaPolicyConfig
+from continual_rl.experiments.environment_runners.full_parallel.environment_runner_full_parallel import EnvironmentRunnerFullParallel
 
 
 class ImpalaPolicy(PolicyBase):
@@ -12,7 +13,9 @@ class ImpalaPolicy(PolicyBase):
         pass
 
     def get_environment_runner(self):
-        pass
+        runner = EnvironmentRunnerFullParallel(self, num_parallel_processes=3, timesteps_per_collection=1000,
+                                               render_collection_freq=50, before_collection_handler=None)
+        return runner
 
     def compute_action(self, observation, task_id, last_info_to_store):
         pass
