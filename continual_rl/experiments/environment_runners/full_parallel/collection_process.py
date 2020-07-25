f5f74a8bafdc11720f5d1c0e76661725d436fec1
Sam Powers <Sam.N.Powers@gmail.com>
2020-07-25 11:56:27 -0400

    Full process runner now has the ability to send and receive data to update the state of the processes.

--- continual_rl/experiments/environment_runners/full_parallel/collection_process.py
+++ continual_rl/experiments/environment_runners/full_parallel/collection_process.py
@@ -5,8 +5,13 @@ import numpy as np
 from continual_rl.experiments.environment_runners.environment_runner_batch import EnvironmentRunnerBatch
 
 
+class StateUpdateUnexpectedError(Exception):
+    pass
+
+
 class CollectionProcess():
-    def __init__(self, policy, timesteps_per_collection, worker_id, seed=None, render_collection_freq=None):
+    def __init__(self, policy, timesteps_per_collection, worker_id, seed=None, render_collection_freq=None,
+                 receive_update_process_bundle=None):
         self.incoming_queue = Queue()  # Requests to process
         self.outgoing_queue = Queue()  # Pass data back to caller
 
@@ -17,6 +22,8 @@ class CollectionProcess():
         self._seed = seed
         if seed is not None:  # TODO test setting a seed and the numpy seeding
             self._seed = seed + worker_id
+
+        self._receive_update_process_bundle = receive_update_process_bundle
 
     def process_queue(self):
         if self._seed is not None:
@@ -44,3 +51,9 @@ class CollectionProcess():
                 results = self._episode_runner.collect_data(time_batch_size, env_spec, preprocessor, task_id,
                                                             episode_renderer, early_stopping_condition)
                 self.outgoing_queue.put(results)
+
+            elif action_id == "update_state":
+                if self._receive_update_process_bundle is not None:
+                    self._receive_update_process_bundle(content)
+                else:
+                    raise StateUpdateUnexpectedError("State update received when none was expected")
--- continual_rl/experiments/environment_runners/full_parallel/environment_runner_full_parallel.py
+++ continual_rl/experiments/environment_runners/full_parallel/environment_runner_full_parallel.py
@@ -9,19 +9,32 @@ class EnvironmentRunnerFullParallel(EnvironmentRunnerBase):
     Runs the entirety of collection on separate processes. Uses pytorch multiprocessing so share_memory() can be used.
     """
     def __init__(self, policy, num_parallel_processes, timesteps_per_collection, render_collection_freq=None,
-                 before_collection_handler=None):
+                 create_update_process_bundle=None, receive_update_process_bundle=None):
+        """
+        create_update_process_bundle is a callback that creates a list of arbitrary data bundles, one per process, to
+        be used to update the states of the processes. This callback is executed on the main process, and the data is
+        passed to each appropriate process.
+        receive_update_process_bundle is the callback that will be called per-process, to consume the data bundle
+        and update accordingly.
+        The form of the data bundles is entirely up to the creator of this EnvironmentRunner.
+        """
         super().__init__()
-        self._before_collection_handler = before_collection_handler
-        self._process_managers = [CollectionProcess(policy, timesteps_per_collection, worker_id, render_collection_freq)
-                           for worker_id in range(num_parallel_processes)]
+        self._create_update_process_bundle = create_update_process_bundle
+        self._process_managers = [CollectionProcess(policy, timesteps_per_collection, worker_id,
+                                                    render_collection_freq=render_collection_freq,
+                                                    receive_update_process_bundle=receive_update_process_bundle)
+                                  for worker_id in range(num_parallel_processes)]
 
         for manager in self._process_managers:
             process = multiprocessing.Process(target=manager.process_queue)  # TODO: if it takes too long, don't do in constructor...also should CollectionProcess have this?
             process.start()
 
-    def send_message_to_all(self, message_id, message_content):
-        for process in self._process_managers:
-            process.incoming_queue.put((message_id, message_content))
+    def _send_message_to_process(self, process_id, message_id, message_content):
+        self._process_managers[process_id].incoming_queue.put((message_id, message_content))
+
+    def _send_message_to_all(self, message_id, message_content):
+        for process_id in range(len(self._process_managers)):
+            self._send_message_to_process(process_id, message_id, message_content)
 
     def collect_data(self, time_batch_size, env_spec, preprocessor, task_id, episode_renderer=None,
                      early_stopping_condition=None):  # TODO: common naming pattern for my lambdas for clarity? (handler?)
@@ -31,8 +44,13 @@ class EnvironmentRunnerFullParallel(EnvironmentRunnerBase):
         """
 
         # Allow the policy to do anything it needs to do before collection (e.g. update the policy on the processes)
-        if self._before_collection_handler is not None:
-            self._before_collection_handler(self)  # Passing self in gives the caller a lot of power (TODO?)
+        # This will trigger the receive_update_process_bundle callback on each process
+        if self._create_update_process_bundle is not None:
+            process_bundles = self._create_update_process_bundle()
+
+            if process_bundles is not None:
+                for process_id, bundle in enumerate(process_bundles):
+                    self._send_message_to_process(process_id, "update_state", bundle)
 
         # All lambdas need to be cloudpickle'd because regular pickle can't handle them
         # Note that env_spec is optionally a lambda
@@ -49,7 +67,7 @@ class EnvironmentRunnerFullParallel(EnvironmentRunnerBase):
         all_rewards_to_report = []
         all_logs_to_report = []
 
-        self.send_message_to_all("start_episode", collection_request_data)
+        self._send_message_to_all("start_episode", collection_request_data)
 
         for process in self._process_managers:
             timesteps, per_timestep_data, rewards_to_report, logs_to_report = process.outgoing_queue.get()
