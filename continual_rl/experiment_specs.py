6a76c89bf4f482eaa0afa0583524a1d5aed4d3b7
Sam Powers <Sam.N.Powers@gmail.com>
2020-08-02 15:56:59 -0400

    Figured doing done-logic in the environment rather than trying to make the task know how to end episodes made more sense.

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -1,14 +1,18 @@
 from continual_rl.experiments.experiment import Experiment
 from continual_rl.experiments.tasks.image_task import ImageTask
 from continual_rl.experiments.tasks.minigrid_task import MiniGridTask
+from continual_rl.utils.env_wrappers import EpisodicLifeEnv
+from continual_rl.utils.utils import Utils
 
 
 def get_available_experiments():
     experiments = {
         "breakout":
             Experiment(tasks=[
-                ImageTask(action_space_id=0, env_spec='BreakoutDeterministic-v4', num_timesteps=10000000, time_batch_size=4,
-                          eval_mode=False, image_size=[84, 84], grayscale=True)
+                ImageTask(action_space_id=0,
+                          env_spec=lambda: EpisodicLifeEnv(Utils.make_env('BreakoutDeterministic-v4')),
+                          num_timesteps=10000000, time_batch_size=4, eval_mode=False,
+                          image_size=[84, 84], grayscale=True)
             ]),
 
         "recall_minigrid_empty8x8_unlock":
