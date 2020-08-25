bba8358df0fff2b1f3500401d76d7ece9b512316
Sam Powers <Sam.N.Powers@gmail.com>
2020-08-25 05:08:35 -0400

    Experiment

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -29,8 +29,7 @@ def get_available_experiments():
         "coinrun_easy_unlimited":
             Experiment(tasks=[
                 ImageTask(action_space_id=0,
-                          env_spec=lambda: VecNormalize(gym.make('procgen:procgen-coinrun-v0', distribution_mode="easy"),
-                                                        gamma=.999),
+                          env_spec=lambda: gym.make('procgen:procgen-coinrun-v0', distribution_mode="easy"),
                           num_timesteps=10000000, time_batch_size=4,
                           eval_mode=False, image_size=[84, 84], grayscale=False)
             ])
