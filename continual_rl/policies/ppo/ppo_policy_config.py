073f5a1e5f063c305fcddc965de7dba44829c1e4
Sam Powers <Sam.N.Powers@gmail.com>
2020-12-22 12:06:03 -0800

    A couple render freq tweaks, to reduce file sizes.

--- continual_rl/policies/ppo/ppo_policy_config.py
+++ continual_rl/policies/ppo/ppo_policy_config.py
@@ -25,7 +25,7 @@ class PPOPolicyConfig(ConfigBase):
         self.use_linear_lr_decay = False
         self.decay_over_steps = 10000000  # The policy shouldn't need to know how long to run, but ... for lr decay...
         self.cuda = True
-        self.render_collection_freq = 10000  # timesteps
+        self.render_collection_freq = 200000  # timesteps
         self.comment = ""  # For experiment-writers to leave a comment for themselves, not used in PPO
 
     def _load_from_dict_internal(self, config_dict):
