85769ee885254a000aa94027ba172ae54fff9a5b
Sam Powers <Sam.N.Powers@gmail.com>
2020-12-30 23:50:52 -0800

    Rescale more intelligently, according to the specs of the obs space. Should probably do this for PPO too.

--- continual_rl/policies/impala/torchbeast/monobeast.py
+++ continual_rl/policies/impala/torchbeast/monobeast.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Taken from https://raw.githubusercontent.com/facebookresearch/torchbeast/3f3029cf3d6d488b8b8f952964795f451a49048f/torchbeast/monobeast.py
-# and modified slightly
+# and modified
 
 import logging
 import os
