d3cbf2210786f14185d71d308606aa6175aeec50
etaoxing <etaoxing@users.noreply.github.com>
2021-01-06 13:07:18 -0500

    separate monobeast loss into its own function

--- continual_rl/policies/impala/torchbeast/monobeast.py
+++ continual_rl/policies/impala/torchbeast/monobeast.py
@@ -248,6 +248,60 @@ class Monobeast():
         timings.time("device")
         return batch, initial_agent_state
 
+    def compute_loss(self, flags, model, batch, initial_agent_state, with_custom_loss=True):
+        learner_outputs, unused_state = model(batch, initial_agent_state)
+
+        # Take final value function slice for bootstrapping.
+        bootstrap_value = learner_outputs["baseline"][-1]
+
+        # Move from obs[t] -> action[t] to action[t] -> obs[t].
+        batch = {key: tensor[1:] for key, tensor in batch.items()}
+        learner_outputs = {key: tensor[:-1] for key, tensor in learner_outputs.items()}
+
+        rewards = batch["reward"]
+        if flags.reward_clipping == "abs_one":
+            clipped_rewards = torch.clamp(rewards, -1, 1)
+        elif flags.reward_clipping == "none":
+            clipped_rewards = rewards
+
+        discounts = (~batch["done"]).float() * flags.discounting
+
+        vtrace_returns = vtrace.from_logits(
+            behavior_policy_logits=batch["policy_logits"],
+            target_policy_logits=learner_outputs["policy_logits"],
+            actions=batch["action"],
+            discounts=discounts,
+            rewards=clipped_rewards,
+            values=learner_outputs["baseline"],
+            bootstrap_value=bootstrap_value,
+        )
+
+        pg_loss = self.compute_policy_gradient_loss(
+            learner_outputs["policy_logits"],
+            batch["action"],
+            vtrace_returns.pg_advantages,
+        )
+        baseline_loss = flags.baseline_cost * self.compute_baseline_loss(
+            vtrace_returns.vs - learner_outputs["baseline"]
+        )
+        entropy_loss = flags.entropy_cost * self.compute_entropy_loss(
+            learner_outputs["policy_logits"]
+        )
+
+        total_loss = pg_loss + baseline_loss + entropy_loss
+        stats = {
+            "pg_loss": pg_loss.item(),
+            "baseline_loss": baseline_loss.item(),
+            "entropy_loss": entropy_loss.item(),
+        }
+
+        if with_custom_loss: # auxilary terms for continual learning
+            custom_loss, custom_stats = self.custom_loss(model, initial_agent_state)
+            total_loss += custom_loss
+            stats.update(custom_stats)
+
+        return total_loss, stats
+
     def learn(
             self,
             flags,
@@ -266,62 +320,18 @@ class Monobeast():
 
             # Prepare the batch for training (e.g. augmenting with more data)
             batch = self.get_batch_for_training(batch)
-            learner_outputs, unused_state = model(batch, initial_agent_state)
 
-            # Take final value function slice for bootstrapping.
-            bootstrap_value = learner_outputs["baseline"][-1]
-
-            # Move from obs[t] -> action[t] to action[t] -> obs[t].
-            batch = {key: tensor[1:] for key, tensor in batch.items()}
-            learner_outputs = {key: tensor[:-1] for key, tensor in learner_outputs.items()}
-
-            rewards = batch["reward"]
-            if flags.reward_clipping == "abs_one":
-                clipped_rewards = torch.clamp(rewards, -1, 1)
-            elif flags.reward_clipping == "none":
-                clipped_rewards = rewards
-
-            discounts = (~batch["done"]).float() * flags.discounting
-
-            vtrace_returns = vtrace.from_logits(
-                behavior_policy_logits=batch["policy_logits"],
-                target_policy_logits=learner_outputs["policy_logits"],
-                actions=batch["action"],
-                discounts=discounts,
-                rewards=clipped_rewards,
-                values=learner_outputs["baseline"],
-                bootstrap_value=bootstrap_value,
-            )
-
-            pg_loss = self.compute_policy_gradient_loss(
-                learner_outputs["policy_logits"],
-                batch["action"],
-                vtrace_returns.pg_advantages,
-            )
-            baseline_loss = flags.baseline_cost * self.compute_baseline_loss(
-                vtrace_returns.vs - learner_outputs["baseline"]
-            )
-            entropy_loss = flags.entropy_cost * self.compute_entropy_loss(
-                learner_outputs["policy_logits"]
-            )
-
-            custom_loss, custom_stats = self.custom_loss(model, initial_agent_state)
-
-            total_loss = pg_loss + baseline_loss + entropy_loss + custom_loss
+            total_loss, stats = self.compute_loss(flags, batch, model, initial_agent_state)
 
             # The episode_return may be nan if we're using an EpisodicLifeEnv (for Atari), where episode_return is nan
             # until the end of the game, where a real return is produced.
             batch_done_flags = batch_for_logging["done"] * ~torch.isnan(batch_for_logging["episode_return"])
             episode_returns = batch_for_logging["episode_return"][batch_done_flags]
-            stats = {
+            stats.update({
                 "episode_returns": tuple(episode_returns.cpu().numpy()),
                 "mean_episode_return": torch.mean(episode_returns).item(),
                 "total_loss": total_loss.item(),
-                "pg_loss": pg_loss.item(),
-                "baseline_loss": baseline_loss.item(),
-                "entropy_loss": entropy_loss.item(),
-            }
-            stats.update(custom_stats)
+            })
 
             optimizer.zero_grad()
             total_loss.backward()
