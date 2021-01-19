509477f1d5490f9e71d015ccc31e37c5416dbed6
Sam Powers <Sam.N.Powers@gmail.com>
2021-01-18 17:48:11 -0800

    Feeling lazy, so just dropping in most of the latest set of experiments, and running one.

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -5,7 +5,7 @@ from continual_rl.utils.env_wrappers import wrap_deepmind, make_atari
 from continual_rl.available_policies import LazyDict
 
 
-def get_single_atari_task(action_space_id, env_name, num_timesteps, max_episode_steps=None):
+def get_single_atari_task(action_space_id, env_name, num_timesteps, max_episode_steps=None, clip_rewards=False):
     """
     Wrap the task creation in a scope so the env_name in the lambda doesn't change out from under us.
     The atari max step default is 100k.
@@ -13,7 +13,7 @@ def get_single_atari_task(action_space_id, env_name, num_timesteps, max_episode_
     return ImageTask(action_space_id=action_space_id,
                      env_spec=lambda: wrap_deepmind(
                          make_atari(env_name, max_episode_steps=max_episode_steps),
-                         clip_rewards=False,
+                         clip_rewards=clip_rewards,
                          frame_stack=False,  # Handled separately
                          scale=False,
                      ),
@@ -21,50 +21,74 @@ def get_single_atari_task(action_space_id, env_name, num_timesteps, max_episode_
                      image_size=[84, 84], grayscale=True)
 
 
-def create_mini_atari_cycle_loader(max_episode_steps, game_names):
+def create_atari_cycle_loader(max_episode_steps, game_names, num_timesteps, continual_testing_freq=5e4):
     return lambda: Experiment(tasks=[
-        get_single_atari_task(action_id, name, num_timesteps=1e7, max_episode_steps=max_episode_steps)
+        get_single_atari_task(action_id, name, num_timesteps=num_timesteps, max_episode_steps=max_episode_steps)
         for action_id, name in enumerate(game_names)
-    ], continual_testing_freq=50000, cycle_count=5)
+    ], continual_testing_freq=continual_testing_freq, cycle_count=5)
 
 
-def create_minier_atari_cycle_loader(max_episode_steps, game_names):
+def create_atari_single_game_loader(env_name, clip_rewards=False):
     return lambda: Experiment(tasks=[
-        get_single_atari_task(action_id, name, num_timesteps=1e5, max_episode_steps=max_episode_steps)
-        for action_id, name in enumerate(game_names)
-    ], continual_testing_freq=50000, cycle_count=5)
-
-
-def create_atari_single_game_loader(env_name):
-    return lambda: Experiment(tasks=[
-        get_single_atari_task(0, env_name, num_timesteps=5e7)
+        get_single_atari_task(0, env_name, num_timesteps=5e7, max_episode_steps=10000, clip_rewards=clip_rewards)
     ])
 
 
-def load_minigrid_empty8x8_unlock():
-    return Experiment(tasks=[MiniGridTask(action_space_id=0, env_spec='MiniGrid-Empty-8x8-v0', num_timesteps=150000,
-                                           time_batch_size=1, eval_mode=False),
-                              MiniGridTask(action_space_id=0, env_spec='MiniGrid-Unlock-v0', num_timesteps=500000,
-                                           time_batch_size=1, eval_mode=False),
-                              MiniGridTask(action_space_id=0, env_spec='MiniGrid-Empty-8x8-v0', num_timesteps=10000,
-                                           time_batch_size=1, eval_mode=True)
-                              ])
-
-
-def load_minigrid_2room_unlock_keycorridor():
-    return Experiment(tasks=[MiniGridTask(action_space_id=0, env_spec='MiniGrid-MultiRoom-N2-S4-v0',
-                                num_timesteps=750000, time_batch_size=1,
-                                eval_mode=False),
-                   MiniGridTask(action_space_id=0, env_spec='MiniGrid-Unlock-v0', num_timesteps=1500000,
-                                time_batch_size=1,
-                                eval_mode=False),
-                   MiniGridTask(action_space_id=0, env_spec='MiniGrid-KeyCorridorS3R1-v0',
-                                num_timesteps=1500000, time_batch_size=1,
+def get_single_minigrid_task(action_space_id, env_name, timesteps):
+    """
+    Wrap the task creation in a scope so the env_name in the lambda doesn't change out from under us.
+    """
+    return MiniGridTask(action_space_id=action_space_id, env_spec=env_name,
+                                num_timesteps=timesteps, time_batch_size=1,
                                 eval_mode=False)
-                   ], continual_testing_freq=10000)
+
+
+def create_minigrid_tasks_loader(task_data, continual_testing_freq=10000):
+    return lambda: Experiment(tasks=[get_single_minigrid_task(*task_info) for task_info in task_data],
+                              continual_testing_freq=continual_testing_freq)
+
+
+def load_easy_coinrun():
+    import gym
+    return Experiment(tasks=[ImageTask(action_space_id=0,
+                                       env_spec=lambda: gym.make("procgen:procgen-coinrun-v0", distribution_mode='easy'),
+                                       num_timesteps=100e6, time_batch_size=4, eval_mode=False, image_size=(84, 84),
+                                       grayscale=True)])
+
+
+def create_easy_coinrun_climber_jumper_loader(num_timesteps):
+    import gym
+    return lambda: Experiment(tasks=[ImageTask(action_space_id=0,
+                                       env_spec=lambda: gym.make("procgen:procgen-coinrun-v0",
+                                                                 distribution_mode='easy'),
+                                       num_timesteps=num_timesteps, time_batch_size=4, eval_mode=False, image_size=(84, 84),
+                                       grayscale=True),
+                             ImageTask(action_space_id=0,
+                                       env_spec=lambda: gym.make("procgen:procgen-climber-v0",
+                                                                 distribution_mode='easy'),
+                                       num_timesteps=num_timesteps, time_batch_size=4, eval_mode=False, image_size=(84, 84),
+                                       grayscale=True),
+                             ImageTask(action_space_id=0,
+                                       env_spec=lambda: gym.make("procgen:procgen-jumper-v0",
+                                                                 distribution_mode='easy'),
+                                       num_timesteps=num_timesteps, time_batch_size=4, eval_mode=False, image_size=(84, 84),
+                                       grayscale=True)
+                             ], continual_testing_freq=50000)
 
 
 def get_available_experiments():
+
+    minigrid_names = ["MiniGrid-Empty-5x5-v0",
+                      "MiniGrid-FourRooms-v0",
+                      "MiniGrid-DoorKey-6x6-v0",
+                      "MiniGrid-MultiRoom-N2-S4-v0",
+                      "MiniGrid-KeyCorridorS3R2-v0",
+                      "MiniGrid-Unlock-v0",
+                      "MiniGrid-UnlockPickup-v0",
+                      "MiniGrid-DistShift1-v0",
+                      "MiniGrid-LavaGapS5-v0",
+                      "MiniGrid-LavaCrossingS9N1-v0",
+                      "MiniGrid-Dynamic-Obstacles-Random-5x5-v0"]
 
     experiments = LazyDict({
         "adventure": create_atari_single_game_loader("AdventureNoFrameskip-v4"),
@@ -124,16 +148,92 @@ def get_available_experiments():
         "yars_revenge": create_atari_single_game_loader("YarsRevengeNoFrameskip-v4"),
         "zaxxon": create_atari_single_game_loader("ZaxxonNoFrameskip-v4"),
 
-        "mini_atari_cycle": create_mini_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
+        "mini_atari_cycle": create_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
+                                                              "KrullNoFrameskip-v4",
+                                                              "BeamRiderNoFrameskip-v4"], num_timesteps=1e7),
+        "mini_atari_cycle_2": create_atari_cycle_loader(10000, ["HeroNoFrameskip-v4",
+                                                                "StarGunnerNoFrameskip-v4",
+                                                                "MsPacmanNoFrameskip-v4"], num_timesteps=1e7),
+        "mini_atari_cycle_full": create_atari_cycle_loader(1e4, ['SpaceInvadersNoFrameskip-v4',
                                                                    "KrullNoFrameskip-v4",
-                                                                   "BeamRiderNoFrameskip-v4"]),
-        "mini_atari_cycle_2": create_mini_atari_cycle_loader(10000, ["HeroNoFrameskip-v4",
-                                                                     "StarGunnerNoFrameskip-v4",
-                                                                     "MsPacmanNoFrameskip-v4"]),
-        "minier_atari_cycle": create_minier_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
+                                                                   "BeamRiderNoFrameskip-v4",
+                                                                   "HeroNoFrameskip-v4",
+                                                                   "StarGunnerNoFrameskip-v4",
+                                                                   "MsPacmanNoFrameskip-v4"], num_timesteps=1e7),
+        "atari_cycle": create_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
+                                                         "KrullNoFrameskip-v4",
+                                                         "BeamRiderNoFrameskip-v4",
+                                                         "HeroNoFrameskip-v4",
+                                                         "StarGunnerNoFrameskip-v4",
+                                                         "MsPacmanNoFrameskip-v4"
+                                                         ], num_timesteps=5e7, continual_testing_freq=200000),
+        "atari_cycle_no_krull": create_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
+                                                                  "BeamRiderNoFrameskip-v4",
+                                                                  "HeroNoFrameskip-v4",
+                                                                  "StarGunnerNoFrameskip-v4",
+                                                                  "MsPacmanNoFrameskip-v4"
+                                                                  ], num_timesteps=5e7, continual_testing_freq=6e6),
+        "minier_atari_cycle": create_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
                                                                    "KrullNoFrameskip-v4",
-                                                                   "BeamRiderNoFrameskip-v4"]),
-        "minigrid_2room_unlock_keycorridor": load_minigrid_2room_unlock_keycorridor
+                                                                   "BeamRiderNoFrameskip-v4"], num_timesteps=1e5),
+        "mini_atari_cycle_6act": create_atari_cycle_loader(10000, ['SpaceInvadersNoFrameskip-v4',
+                                                                   "PongNoFrameskip-v4",
+                                                                   "QbertNoFrameskip-v4"], num_timesteps=5e6),
+        "minigrid_2room_unlock": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
+                                                               (0, 'MiniGrid-Unlock-v0', 1500000)]),
+        "minigrid_2room_lavagap5_unlock": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 750000),
+                                                                        (0, 'MiniGrid-Unlock-v0', 1500000)]),
+        "minigrid_2room_lavagap7_unlock": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS7-v0', 1000000),
+                                                                        (0, 'MiniGrid-Unlock-v0', 1500000)]),
+        "minigrid_2room_unlock_lavacurric": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
+                                                                        (0, 'MiniGrid-Unlock-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N1-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N2-v0', 1500000)]),
+        "minigrid_2room_obstacles_lavacurric": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
+                                                                        (1, 'MiniGrid-Dynamic-Obstacles-Random-5x5-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N1-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N2-v0', 1500000)]),
+
+        "minigrid_obstacles_lavacurric": create_minigrid_tasks_loader([(1, 'MiniGrid-Dynamic-Obstacles-Random-5x5-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 1500000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N1-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N2-v0', 1500000)]),
+        "minigrid_obstacles8_lavacurric": create_minigrid_tasks_loader([(1, 'MiniGrid-Dynamic-Obstacles-8x8-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 1500000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N1-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaCrossingS9N2-v0', 1500000)]),
+        "minigrid_obstacles_distshift": create_minigrid_tasks_loader([(1, 'MiniGrid-Dynamic-Obstacles-Random-5x5-v0', 750000),
+                                                                        (0, 'MiniGrid-DistShift1-v0', 750000)]),
+        "minigrid_obstacles_unlock": create_minigrid_tasks_loader([(1, 'MiniGrid-Dynamic-Obstacles-Random-5x5-v0', 750000),
+                                                                        (0, "MiniGrid-Unlock-v0", 1500000)]),
+        "minigrid_obstacles_keycorridor": create_minigrid_tasks_loader([(1, 'MiniGrid-Dynamic-Obstacles-Random-5x5-v0', 750000),
+                                                                        (0, "MiniGrid-KeyCorridorS3R2-v0", 1500000)]),
+
+        "minigrid_unlock_obstacles": create_minigrid_tasks_loader([(1, 'MiniGrid-Unlock-v0', 750000),
+                                                                   (0, "MiniGrid-Dynamic-Obstacles-Random-5x5-v0",
+                                                                    1500000)]),
+        "minigrid_empty_obstacles": create_minigrid_tasks_loader([(1, 'MiniGrid-Empty-5x5-v0', 750000),
+                                                                        (0, "MiniGrid-Dynamic-Obstacles-Random-5x5-v0", 1500000)]),
+        "minigrid_empty8_obstacles": create_minigrid_tasks_loader([(1, 'MiniGrid-Empty-8x8-v0', 500000),
+                                                                  (0, "MiniGrid-Dynamic-Obstacles-Random-5x5-v0",
+                                                                   1500000)]),
+        "minigrid_empty8_obstacles_lavaS5": create_minigrid_tasks_loader([(0, 'MiniGrid-Empty-8x8-v0', 300000),
+                                                                        (1, "MiniGrid-Dynamic-Obstacles-Random-5x5-v0", 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 1500000)]),
+
+        "minigrid_2room_lavagap5_obstacles": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
+                                                                        (0, 'MiniGrid-LavaGapS5-v0', 750000),
+                                                                        (1, 'MiniGrid-Dynamic-Obstacles-Random-5x5-v0', 750000)]),
+
+        "easy_coinrun": load_easy_coinrun,
+        "easy_coinrun_climber_jumper": create_easy_coinrun_climber_jumper_loader(30e6),
+        "easy_coinrun_climber_jumper_short": create_easy_coinrun_climber_jumper_loader(5e6),
+
+        "hero_clip_rewards": create_atari_single_game_loader("HeroNoFrameskip-v4", clip_rewards=True)
     })
 
     return experiments
