b4c50b963d3fb7c49a015bc06d5c8512a4288b00
Sam Powers <Sam.N.Powers@gmail.com>
2021-01-22 00:18:31 -0800

    First stab at actually having progress and compress phases.

--- continual_rl/policies/progress_and_compress/progress_and_compress_monobeast.py
+++ continual_rl/policies/progress_and_compress/progress_and_compress_monobeast.py
@@ -10,6 +10,7 @@ class ProgressAndCompressMonobeast(EWCMonobeast):
     """
     def __init__(self, model_flags, observation_space, action_spaces, policy_class):
         super().__init__(model_flags, observation_space, action_spaces, policy_class)
+        self._train_steps_since_boundary = None
 
     def _compute_kl_div_loss(self, input, target):
         # KLDiv requires inputs to be log-probs, and targets to be probs
@@ -18,7 +19,7 @@ class ProgressAndCompressMonobeast(EWCMonobeast):
         kl_loss = torch.nn.KLDivLoss(reduction='sum')(curr_log_policy, old_policy.detach())
         return kl_loss
 
-    def custom_loss(self, model, initial_agent_state):
+    def knowledge_base_loss(self, model, initial_agent_state):
         ewc_loss, ewc_stats = super().custom_loss(model.knowledge_base, initial_agent_state)
 
         # Additionally, minimize KL divergence between KB and active column (only updating KB)
@@ -34,3 +35,27 @@ class ProgressAndCompressMonobeast(EWCMonobeast):
         ewc_stats.update({"kl_div_loss": kl_div_loss})
 
         return total_loss, ewc_stats
+
+    def compute_loss(self, flags, model, batch, initial_agent_state, with_custom_loss=True):
+        """
+        Sometimes we want to turn off normal loss computation entirely, so controlling that here.
+        During the "wake" part of the cycle, we use the normal compute_loss to update the active column (AC).
+        During "sleep" we use EWC+KL to update the knowledge base (KB).
+        The P&C paper is not very clear on cadence/length of the phases, so we're assuming sleep starts at
+        the end of a task, and lasts a hyperparameter number of learning steps (num_train_steps_of_compress).
+        We are assuming it is happening alongside continued data collection because the paper references "rewards
+        collected during the compress phase".
+        """
+        if self._train_steps_since_boundary is None or self._train_steps_since_boundary > self._model_flags.num_train_steps_of_compress:
+            # This is the "active column" training setting
+            results = super().compute_loss(flags, model, batch, initial_agent_state, with_custom_loss=False)
+        else:
+            # This is the "knowledge base" training setting
+            results = self.knowledge_base_loss(model, initial_agent_state)
+
+        return results
+
+    def set_current_task(self, task_id):
+        super().set_current_task(task_id)
+
+
--- continual_rl/policies/progress_and_compress/progress_and_compress_policy_config.py
+++ continual_rl/policies/progress_and_compress/progress_and_compress_policy_config.py
@@ -5,3 +5,4 @@ class ProgressAndCompressPolicyConfig(OnlineEWCPolicyConfig):
 
     def __init__(self):
         super().__init__()
+        self.num_train_steps_of_compress = 1000
