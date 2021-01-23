891a65c8fdd719a93c494beaecaa0ec87493fe9f
Sam Powers <Sam.N.Powers@gmail.com>
2021-01-22 23:51:27 -0800

    Variant...

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -281,6 +281,9 @@ def get_available_experiments():
         "minigrid_empty8_obstacles5_unlockpickup": create_minigrid_tasks_loader([(0, 'MiniGrid-Empty-8x8-v0', 300000),
                                                                         (1, "MiniGrid-Dynamic-Obstacles-Random-5x5-v0", 750000),
                                                                         (0, 'MiniGrid-UnlockPickup-v0', 1500000)]),
+        "minigrid_empty8_obstacles5_keycorridor": create_minigrid_tasks_loader([(0, 'MiniGrid-Empty-8x8-v0', 300000),
+                                                                        (1, "MiniGrid-Dynamic-Obstacles-Random-5x5-v0", 750000),
+                                                                        (0, 'MiniGrid-KeyCorridorS3R1-v0', 1500000)]),
 
         "minigrid_2room_lavagap5_obstacles": create_minigrid_tasks_loader([(0, 'MiniGrid-MultiRoom-N2-S4-v0', 750000),
                                                                         (0, 'MiniGrid-LavaGapS5-v0', 750000),
