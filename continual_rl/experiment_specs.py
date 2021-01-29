48c4b4fb880bf379ba2bf2d10504a74c40fd4570
Sam Powers <Sam.N.Powers@gmail.com>
2021-01-29 01:34:34 -0800

    May as well complete the set.

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -356,6 +356,20 @@ def get_available_experiments():
                  1000000, 1, True)
             ]
         ),
+        "minigrid_association_4_invis_colorfuldistract_squeeze": create_minigrid_tasks_loader(
+            [
+                (0, lambda: AssociationEnv(association_pairs=[('purple', 'yellow'),
+                                                              ('green', 'blue'),
+                                                                 ('yellow', 'green'),
+                                                              ('blue', 'purple')], indicator_color='red', squeeze_together=True),
+                 1000000, 1, True),  # "invisible" indicator
+                (0, lambda: AssociationEnv(association_pairs=[('purple', 'green'),
+                                                              ('green', 'purple'),
+                                                                 ('yellow', 'blue'),
+                                                                 ('blue', 'yellow')], indicator_color='grey', squeeze_together=True),
+                 1000000, 1, True)
+            ]
+        ),
         "minigrid_association_2_match_invis1_1distract": create_minigrid_tasks_loader(
             [
                 (0, lambda: AssociationEnv(association_pairs=[('purple', 'yellow'),
