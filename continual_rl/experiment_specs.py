11fad96831c6352f43e3a8e2c7a845a2bea2ff82
Sam Powers <Sam.N.Powers@gmail.com>
2021-01-29 01:32:46 -0800

    Different order.

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -342,6 +342,20 @@ def get_available_experiments():
                  1000000, 1, True)
             ]
         ),
+        "minigrid_association_3_invis_colorfuldistract_squeeze2": create_minigrid_tasks_loader(
+            [
+                (0, lambda: AssociationEnv(association_pairs=[('purple', 'yellow'),
+                                                              ('green', 'blue'),
+                                                                 ('yellow', 'green'),
+                                                                 (None, 'purple')], indicator_color='red', squeeze_together=True),
+                 1000000, 1, True),  # "invisible" indicator
+                (0, lambda: AssociationEnv(association_pairs=[('purple', 'green'),
+                                                              ('green', 'purple'),
+                                                                 ('yellow', 'blue'),
+                                                                 (None, 'yellow')], indicator_color='grey', squeeze_together=True),
+                 1000000, 1, True)
+            ]
+        ),
         "minigrid_association_2_match_invis1_1distract": create_minigrid_tasks_loader(
             [
                 (0, lambda: AssociationEnv(association_pairs=[('purple', 'yellow'),
