f8072667b3a00b2de801e7e27da0b0f04d7341d3
Sam Powers <Sam.N.Powers@gmail.com>
2021-07-13 17:46:54 -0400

    Missing dependency.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
                       'torch-ac',
                       'gym-minigrid>=1.0.2',
                       'gym[atari]',
+                      'atari-py',
                       'moviepy',
                       'dotmap',
                       'procgen',
