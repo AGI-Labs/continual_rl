fab420ddce86d473a9ae7f02135ef819069a43f9
Sam Powers <Sam.N.Powers@gmail.com>
2021-08-16 14:12:39 -0400

    Wrong addition method

--- continual_rl/policies/impala/impala_environment_runner.py
+++ continual_rl/policies/impala/impala_environment_runner.py
@@ -123,7 +123,7 @@ class ImpalaEnvironmentRunner(EnvironmentRunnerBase):
             if "video" in stats and stats["video"] is not None:
                 video_log = self._render_video(task_spec.preprocessor, stats["video"])
                 if video_log is not None:
-                    logs_to_report.append(video_log)
+                    logs_to_report.extend(video_log)
 
         return timesteps, all_env_data, rewards_to_report, logs_to_report
 
