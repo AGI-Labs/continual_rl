720f4734f245b09560393a811cadca856dd19e0c
Sam Powers <snpowers@cs.cmu.edu>
2022-05-05 15:08:37 -0400

    Import properly

--- continual_rl/experiment_specs.py
+++ continual_rl/experiment_specs.py
@@ -145,48 +145,51 @@ def create_continuous_control_tasks_loader(task_name, num_timesteps=10e6, contin
 
 
 def create_continuous_control_tasks_loader_pymultigoal(task_name, num_timesteps=10e6, continual_testing_freq=10000, cycle_count=1):
-    #import pybullet_multigoal_gym as pmg
-    # Install matplotlib if you want to use imshow to view the goal images
-    import matplotlib.pyplot as plt
+    def create_env():
+        import pybullet_multigoal_gym as pmg
+        # Install matplotlib if you want to use imshow to view the goal images
+        import matplotlib.pyplot as plt
 
-    camera_setup = [  # TODO: not sure why it's doubled. Also the 84 dim was originally 128
-        {
-            'cameraEyePosition': [-1.0, 0.25, 0.6],
-            'cameraTargetPosition': [-0.6, 0.05, 0.2],
-            'cameraUpVector': [0, 0, 1],
-            'render_width': 84,
-            'render_height': 84
-        },
-        {
-            'cameraEyePosition': [-1.0, -0.25, 0.6],
-            'cameraTargetPosition': [-0.6, -0.05, 0.2],
-            'cameraUpVector': [0, 0, 1],
-            'render_width': 84,
-            'render_height': 84
-        }
-    ]
+        camera_setup = [  # TODO: not sure why it's doubled. Also the 84 dim was originally 128
+            {
+                'cameraEyePosition': [-1.0, 0.25, 0.6],
+                'cameraTargetPosition': [-0.6, 0.05, 0.2],
+                'cameraUpVector': [0, 0, 1],
+                'render_width': 84,
+                'render_height': 84
+            },
+            {
+                'cameraEyePosition': [-1.0, -0.25, 0.6],
+                'cameraTargetPosition': [-0.6, -0.05, 0.2],
+                'cameraUpVector': [0, 0, 1],
+                'render_width': 84,
+                'render_height': 84
+            }
+        ]
 
-    env_fn = lambda: pmg.make_env(
-        # task args ['reach', 'push', 'slide', 'pick_and_place',
-        #            'block_stack', 'block_rearrange', 'chest_pick_and_place', 'chest_push']
-        task='block_stack',
-        gripper='parallel_jaw',
-        num_block=4,  # only meaningful for multi-block tasks, up to 5 blocks
-        render=False,
-        binary_reward=True,
-        max_episode_steps=50,
-        # image observation args
-        image_observation=True,
-        depth_image=False,
-        goal_image=True,
-        visualize_target=True,
-        camera_setup=camera_setup,
-        observation_cam_id=[0],
-        goal_cam_id=1)
+        env_fn = lambda: pmg.make_env(
+            # task args ['reach', 'push', 'slide', 'pick_and_place',
+            #            'block_stack', 'block_rearrange', 'chest_pick_and_place', 'chest_push']
+            task='block_stack',
+            gripper='parallel_jaw',
+            num_block=4,  # only meaningful for multi-block tasks, up to 5 blocks
+            render=False,
+            binary_reward=True,
+            max_episode_steps=50,
+            # image observation args
+            image_observation=True,
+            depth_image=False,
+            goal_image=True,
+            visualize_target=True,
+            camera_setup=camera_setup,
+            observation_cam_id=[0],
+            goal_cam_id=1)
 
-    return lambda: Experiment(tasks=[ImageTask(task_name, action_space_id=0, env_spec=env_fn, num_timesteps=num_timesteps,
-                                               time_batch_size=1, eval_mode=False, image_size=[84, 84], grayscale=False)],
-                              continual_testing_freq=continual_testing_freq, cycle_count=cycle_count)
+        return Experiment(tasks=[ImageTask(task_name, action_space_id=0, env_spec=env_fn, num_timesteps=num_timesteps,
+                                                   time_batch_size=1, eval_mode=False, image_size=[84, 84], grayscale=False)],
+                                  continual_testing_freq=continual_testing_freq, cycle_count=cycle_count)
+
+    return create_env
 
 
 def create_continuous_control_state_tasks_loader(task_name, num_timesteps=10e6, continual_testing_freq=10000, cycle_count=1):
