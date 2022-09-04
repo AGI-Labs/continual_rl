ed86c68db0f9277156935b351fbd5c36367652f0
Sam Powers <spowers@fb.com>
2022-09-04 15:09:46 -0400

    Structure slurm differently.

--- submitit_main.py
+++ submitit_main.py
@@ -5,14 +5,8 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from continual_rl.utils.argparse_manager import ArgparseManager
 
 
-if __name__ == "__main__":
-    executor = submitit.AutoExecutor(folder="tmp/results")
-
-    # set timeout in min, and partition for running the job
-    executor.update_parameters(timeout_min=24*60*3, slurm_partition="dev")
-
+def start(args):
     # Pytorch multiprocessing requires either forkserver or spawn.
-    # TODO: Does this get picked up by submitit properly? (Probably not...)
     try:
         multiprocessing.set_start_method("spawn")
     except ValueError as e:
@@ -20,15 +14,22 @@ if __name__ == "__main__":
         assert "cannot find context" in str(e)
         multiprocessing.set_start_method("spawn")
 
-    experiment, policy = ArgparseManager.parse(sys.argv[1:])
+    experiment, policy = ArgparseManager.parse(args)
 
     if experiment is None:
         raise RuntimeError("No experiment started. Most likely there is no new run to start.")
 
     summary_writer = SummaryWriter(log_dir=experiment.output_dir)
-    #experiment.try_run(policy, summary_writer=summary_writer)
+    experiment.try_run(policy, summary_writer=summary_writer)
 
-    job = executor.submit(experiment.try_run, policy, summary_writer=summary_writer)
+
+if __name__ == "__main__":
+    executor = submitit.AutoExecutor(folder="tmp/results")
+
+    # set timeout in min, and partition for running the job
+    executor.update_parameters(timeout_min=24*60*3, slurm_partition="dev")
+
+    job = executor.submit(start, sys.argv[1:])
     print(job.job_id)  # ID of your job
 
     output = job.result()  # waits for completion and returns output
