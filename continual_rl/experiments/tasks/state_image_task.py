e3f9d803c540141a242e4ad32e1e8bff3004f50e
Sam Powers <spowers@fb.com>
2022-09-04 13:24:17 -0400

    Clean up immediately.

--- continual_rl/experiments/tasks/state_image_task.py
+++ continual_rl/experiments/tasks/state_image_task.py
@@ -9,8 +9,13 @@ from continual_rl.utils.env_wrappers import FrameStack, WarpFrame, ImageToPyTorc
 class StateImagePreprocessor(PreprocessorBase):
     def __init__(self, time_batch_size, image_size, grayscale, env_spec, resize_interp_method):
         self.env_spec = self._wrap_env(env_spec, time_batch_size, image_size, grayscale, resize_interp_method)
+
+        # Clean up the dummy env immediately because multiple creation causes issues with some envs
         dummy_env, _ = Utils.make_env(self.env_spec)
-        super().__init__(dummy_env.observation_space)
+        observation_space = dummy_env.observation_space
+        del dummy_env
+
+        super().__init__(observation_space)
 
     def _wrap_env(self, env_spec, time_batch_size, image_size, grayscale, resize_interp_method):
         def env_wrapper(env):
@@ -48,8 +53,12 @@ class StateImageTask(TaskBase):
                  image_size, grayscale, continual_eval=True, resize_interp_method="INTER_AREA",
                  demonstration_task=False):
         preprocessor = StateImagePreprocessor(time_batch_size, image_size, grayscale, env_spec, resize_interp_method)
+
+        # Clean up the dummy env immediately because multiple creation causes issues with some envs
         dummy_env, _ = Utils.make_env(preprocessor.env_spec)
+        action_space = dummy_env.action_space
+        del dummy_env
 
         super().__init__(task_id, action_space_id, preprocessor, preprocessor.env_spec, preprocessor.observation_space,
-                         dummy_env.action_space, num_timesteps, eval_mode, continual_eval=continual_eval,
+                         action_space, num_timesteps, eval_mode, continual_eval=continual_eval,
                          demonstration_task=demonstration_task)
