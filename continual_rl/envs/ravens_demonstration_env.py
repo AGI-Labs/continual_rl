Wrong ratio and int depths made the fused heightmap wrong. Also switching back to fully loading trajectories rather than datasets samples, because I think I had an observation off by one error and this is the easiest fix offhand.

continual_rl/envs/ravens_demonstration_env.py
@@ -64,23 +64,34 @@ class RavensSimEnvironment(gym.Env):
         converted_observation = {"image": np.concatenate(all_camera_data, axis=-1)}
         return converted_observation
 
+    @staticmethod
+    def convert_unified_action_to_dict(unified_action):
+        action_dict = {}
+        action_index = 0
+
+        for pose_id in ("pose0", "pose1"):
+            action_dict[pose_id] = []
+            for space_dim in (3, 4):  # xyz, xyzw - TODO: not hard-coded
+                action_dict[pose_id].append(unified_action[action_index : action_index + space_dim])
+                action_index += space_dim
+
+        return action_dict
+
+    def _convert_dict_to_unified_action(self, dict_action):
+        unified_action = []
+        for pose_id in ("pose0", "pose1"):
+            for space in dict_action[pose_id]:
+                unified_action.append(space)
+
+        return torch.tensor(np.concatenate(unified_action))  # TODO: this really shouldn't convert to torch here, but it is very convenient
+
     def reset(self):
         observation = self._env.reset()
         self._current_step = 0
         return self._convert_observation(observation)
 
     def step(self, action):
-        converted_action = {}
-        current_action_start = 0
-        for pose_id in ("pose0", "pose1"):  # TODO: de-dupe with converter in demonstration env
-            pose_actions = []
-            for space in self._env.action_space[pose_id].spaces:
-                space_size = space.shape[0]
-                pose_actions.append(action[current_action_start:current_action_start+space_size])
-                current_action_start += space_size
-
-            converted_action[pose_id] = tuple(pose_actions)
-
+        converted_action = self.convert_unified_action_to_dict(action)
         observation, reward, done, info = self._env.step(converted_action)
 
         done = done or self._current_step >= self._max_steps
@@ -115,27 +126,6 @@ class RavensDemonstrationEnv(RavensSimEnvironment):
 
         self._current_timestep = 0
 
-    def _convert_dict_to_unified_action(self, dict_action):
-        unified_action = []
-        for pose_id in ("pose0", "pose1"):
-            for space in dict_action[pose_id]:
-                unified_action.append(space)
-
-        return torch.tensor(np.concatenate(unified_action))  # TODO: this really shouldn't convert to torch here, but it is very convenient
-
-    @staticmethod
-    def convert_unified_action_to_dict(unified_action):
-        action_dict = {}
-        action_index = 0
-
-        for pose_id in ("pose0", "pose1"):
-            action_dict[pose_id] = []
-            for space_dim in (3, 4):  # xyz, xyzw - TODO: not hard-coded
-                action_dict[pose_id].append(unified_action[action_index : action_index + space_dim])
-                action_index += space_dim
-
-        return action_dict
-
     def reset(self):
         self._current_trajectory_id = random.randint(0, len(self._trajectory_ids)-1)
         trajectory_filename = self._trajectory_ids[self._current_trajectory_id]
@@ -162,20 +152,24 @@ class RavensDemonstrationEnv(RavensSimEnvironment):
         return self._convert_observation(observation)
 
     def step(self, action):
-        (obs, act, reward, _), (goal_obs, _, _, _)  = self._dataset.sample()
+        """(obs, act, reward, _), (goal_obs, _, _, _)  = self._dataset.sample()
         demo_action = self._convert_dict_to_unified_action(act)
         info = {"demo_action": demo_action}
 
         done = self._current_step >= self._max_steps  # Never hits this: np.all(obs["color"] == goal_obs["color"]) and np.all(obs["depth"] == goal_obs["depth"])  # TODO: this isn't really necessary...just doing it for video rendering purposes mostly
         self._current_step += 1
 
-        obs = self._convert_observation(obs)
+        obs = self._convert_observation(obs)"""
 
-        """observation = self._convert_observation(self._current_colors[self._current_timestep])
+        raw_obs = {"color": self._current_colors[self._current_timestep + 1], "depth": self._current_depths[self._current_timestep + 1]}
+        observation = self._convert_observation(raw_obs)
+        demo_action = self._current_actions[self._current_timestep]
+        demo_action = self._convert_dict_to_unified_action(demo_action)
+
         info = {"demo_action": demo_action}  # Just has pose0 and pose1, which I already have from action? TODO: self._current_infos[self._current_timestep]
         reward = self._current_rewards[self._current_timestep]
         done = self._current_timestep + 1 == len(self._current_actions) or self._current_actions[self._current_timestep + 1] is None
 
-        self._current_timestep += 1"""
+        self._current_timestep += 1
 
-        return obs, reward, done, info
+        return observation, reward, done, info

continual_rl/experiments/tasks/image_task.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from continual_rl.experiments.tasks.task_base import TaskBase
 from continual_rl.experiments.tasks.preprocessor_base import PreprocessorBase
 from continual_rl.utils.utils import Utils
@@ -19,11 +20,12 @@ class ImagePreprocessor(PreprocessorBase):
         super().__init__(observation_space)
 
     def _wrap_env(self, env_spec, time_batch_size, image_size, grayscale):
+        # TODO: np.float32 is to handle depth (which is a float) - hacky
         # Leverage the existing env wrappers for simplicity
         frame_stacked_env_spec = lambda: FrameStack(ImageToPyTorch(
             WarpFrame(Utils.make_env(env_spec)[0], image_size[0], image_size[1], grayscale=grayscale,
                       resize_interp_method=self._resize_interp_method, dict_space_key=self._dict_space_key),
-            dict_space_key=self._dict_space_key), time_batch_size)
+            dict_space_key=self._dict_space_key, dtype=np.float32), time_batch_size)
         return frame_stacked_env_spec
 
     def preprocess(self, batched_env_image):

continual_rl/policies/impala/nets.py
@@ -9,8 +9,7 @@ import numpy as np
 from continual_rl.utils.common_nets import get_network_for_size
 from continual_rl.utils.utils import Utils
 from continual_rl.policies.impala.random_process import OrnsteinUhlenbeckProcess
-from ravens_torch.agents.transporter import TransporterAgent, Attention, Transport
-from ravens_torch.utils import utils
+from ravens_torch.agents.transporter import OriginalTransporterAgent
 
 
 class ImpalaNet(nn.Module):
@@ -302,30 +301,6 @@ class ContinuousImpalaNet(ImpalaNet):
         )
 
 
-class OriginalTransporterAgent(TransporterAgent):
-
-    def __init__(self, name, task, root_dir, n_rotations=36, verbose=False):
-        super().__init__(name, task, root_dir, n_rotations)
-
-        self.attention = Attention(
-            in_shape=self.in_shape,
-            n_rotations=1,
-            preprocess=utils.preprocess,
-            verbose=verbose)
-        self.transport = Transport(
-            in_channels=self.in_shape[2],
-            n_rotations=self.n_rotations,
-            crop_size=self.crop_size,
-            preprocess=utils.preprocess,
-            verbose=verbose)
-
-    """def parameters(self):
-        parameters = []
-        parameters.extend(self.attention.model.parameters())
-        parameters.extend(self.transport.model_key.parameters())
-        return parameters"""
-
-
 class TransporterImpalaNet(ImpalaNet):
     def __init__(self, observation_space, action_spaces, model_flags, conv_net=None):
         super().__init__(observation_space, action_spaces, model_flags, conv_net, skip_net_init=True)
@@ -335,11 +310,10 @@ class TransporterImpalaNet(ImpalaNet):
         first_action_space = list(action_spaces.values())[0]
         self.num_actions = first_action_space.shape[0]
 
-        self.agent = OriginalTransporterAgent(name="transporter_net", task=None, root_dir=model_flags.output_dir)
+        self.agent = OriginalTransporterAgent(name="transporter_net", task=None, root_dir=model_flags.output_dir, learning_rate=model_flags.actor_learning_rate)
 
     def parameters(self):
         return self.agent.parameters()
-
 
     # TODO:  move these conversion methods to a standard place
     def _convert_dict_to_unified_action(self, dict_action):

continual_rl/utils/env_wrappers.py
@@ -420,7 +420,7 @@ class ImageToPyTorch(gym.ObservationWrapper):
     # As in, LazyFrames provides no benefit?
     # For now switching this to return a Tensor and calling it *before* FrameStack...
 
-    def __init__(self, env, dict_space_key=None):
+    def __init__(self, env, dict_space_key=None, dtype=np.uint8):
         super(ImageToPyTorch, self).__init__(env)
         self._key = dict_space_key
 
@@ -434,7 +434,7 @@ class ImageToPyTorch(gym.ObservationWrapper):
             low=0,
             high=255,
             shape=(old_shape[-1], old_shape[0], old_shape[1]),
-            dtype=np.uint8,
+            dtype=dtype,
         )
         if self._key is None:
             self.observation_space = new_space